0c2ee122012e30d6e73bf0d0e9c2e002da9db05e
github-guo <547122965@qq.com>
2016-01-28 10:41:55 +0800

    test Chinese content

--- SupportPro/src/com/controller/TestMail.java
+++ SupportPro/src/com/controller/TestMail.java
@@ -1,23 +1,28 @@
 package com.controller;
 
+import java.util.Properties;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 public class TestMail {
 	public static void main(String[] args) {
 		JavaMailSenderImpl sender = new JavaMailSenderImpl();
-		sender.setUsername("547122965@qq.com");
-		sender.setPassword("llc772332");
+		sender.setUsername("dingmm@vvaccess.com");
+		sender.setPassword("7876574dmm!");
 		sender.setDefaultEncoding("utf-8");
-		sender.setHost("smtp.qq.com");
-		sender.setPort(587);
-	
+		sender.setHost("smtp.exmail.qq.com");
+		sender.setPort(465);
+		Properties pro = new Properties();
+		pro.setProperty("mail.smtp.timeout", "10000");
+		sender.setJavaMailProperties(pro);
 		SimpleMailMessage msg =new SimpleMailMessage();
-		msg.setFrom("547122965@qq.com");
-		msg.setTo("157449192@qq.com");
+		msg.setFrom("dingmm@vvaccess.com");
+		msg.setTo("547122965@qq.com");
 		msg.setText("你好 这是一份测试邮件");
 		msg.setSubject("阿克斯科技有限公司");
-		msg.setCc("547122965@qq.com");
+		msg.setCc("157449192@qq.com");
+		
 		sender.send(msg);
 	}
 }
